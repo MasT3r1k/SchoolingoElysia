a8ece9a26a5d9b591e7ad114257fe898095668f6
MasT3r1k <master0123@centrum.cz>
2025-11-21 15:00:46 +0100

    Classbook update

---
+++ src/routes/api/v1/classbook/add_absence.ts
@@ -0,0 +1,106 @@
+import { Elysia, t } from 'elysia';
+import { db } from "../../../../../database";
+import { rateLimit } from 'elysia-rate-limit';
+import { app } from '../../../../../index';
+import moment from 'moment';
+
+const elysiaApp = new Elysia()
+  .use(rateLimit({
+    scoping: "scoped",
+    max: 10,
+    duration: 1000,
+    injectServer: () => app.server
+  }))
+  .post('/classbook/absence', async ({ cookie, body }) => {
+    // === AUTH ===
+    const token = cookie.token.value;
+    if (!token) return { error: 'no_user', details: 'no_cookie' };
+
+    const user = await db.selectFrom("tokens")
+      .innerJoin('users', 'users.userId', 'tokens.userId')
+      .innerJoin("passwords", "passwords.passwordId", "users.password")
+      .select([
+        'users.userId',
+        'users.username',
+        'users.person',
+        'users.2fa',
+        'users.2fa_secret',
+        'passwords.password'
+      ])
+      .where('tokens.token', '=', token)
+      .where('tokens.expires', '>=', moment().toDate())
+      .executeTakeFirst();
+
+    if (!user) return { error: 'no_user', details: 'no_db' };
+
+    // === TEACHER PERMISSION ===
+    const perm = await db.selectFrom('teachers')
+      .select(['teachers.personId'])
+      .where('personId', '=', user.person)
+      .executeTakeFirst();
+
+    if (!perm) return { error: 'no_permission' };
+
+    // === VALIDACE QUERY ===
+    const { classbook_id, student_id, type, reason, minutes, note } = body;
+    if (
+           classbook_id == undefined
+        || student_id == undefined
+        || type == undefined
+        || reason == undefined
+        || minutes == undefined
+        || note == undefined) return { error: 'bad_query' };
+
+    const isExistAbsence = await db.selectFrom('absence')
+    .select([
+        'absence.lesson',
+        'absence.student',
+    ])
+    .where('absence.lesson', '=', classbook_id)
+    .where('absence.student', '=', student_id)
+    .executeTakeFirst()
+
+    if (!isExistAbsence && type >= 0) {
+        await db.insertInto('absence')
+        .values({
+            lesson: classbook_id,
+            student: student_id,
+            type,
+            reason,
+            minutes,
+            note
+        })
+        .execute();
+    } else {
+        if (type >= 0) {
+            await db.updateTable('absence')
+            .set('absence.type', type)
+            .set('absence.minutes', minutes)
+            .set('absence.note', note)
+            .set('absence.reason', reason)
+            .where('absence.lesson', '=', classbook_id)
+            .where('absence.student', '=', student_id)
+            .execute()
+        } else {
+            await db.deleteFrom('absence')
+            .where('absence.lesson', '=', classbook_id)
+            .where('absence.student', '=', student_id)
+            .limit(1)
+            .execute();
+        }
+    }
+
+    return { success: true };
+
+  }, {
+    body: t.Object({
+      student_id: t.Optional(t.Number()),
+      classbook_id: t.Optional(t.Number()),
+      type: t.Optional(t.Number()),
+      reason: t.Optional(t.String()),
+      minutes: t.Optional(t.Number()),
+      note: t.Optional(t.String()),
+    })
+  });
+
+export default elysiaApp;
